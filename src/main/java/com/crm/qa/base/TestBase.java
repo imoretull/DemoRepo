863dc7f775d9b394e2179930b871e69956dc3c61
imoretull <kiran.paruchuru@gmail.com>
2018-06-11 15:11:28 -0400

    base prop and initialization method

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -1,5 +1,50 @@
 package com.crm.qa.base;
 
-public class TestBase {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestBase {
+	
+	public static WebDriver driver;
+	public static Properties prop;
+	
+	public TestBase(){
+		try {
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+ 
+					"/src/main/java/com/crm/qa/config/config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void initialization(){
+		String browserName = prop.getProperty("browser");
+		
+		if(browserName.equals("chrome")){
+			// possibly refactor the path and store .exe at project level?
+			System.setProperty("webdriver.chrome.driver", "D:\\myDevelopment\\installs\\drivers\\chromedriver.exe");
+			driver = new ChromeDriver(); 
+		}
+		else if(browserName.equals("FF")){
+			System.setProperty("webdriver.gecko.driver", "D:\\myDevelopment\\installs\\drivers\\geckodriver.exe");
+			driver = new FirefoxDriver(); 
+		}
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		
+		driver.get(prop.getProperty("url"));
+	}
 }
